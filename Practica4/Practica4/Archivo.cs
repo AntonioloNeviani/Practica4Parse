635db50c030a9b7439e1c0038b4e13cd5ac7441f
AntonioloNeviani <jorgeant2000@hotmail.com>
2013-10-23 02:05:50 -0500

    Archivo Parse CSV

---
+++ Practica4/Practica4/Archivo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4
+{
+    class Archivo
+    {
+        private string @rutaDeArchivo;
+
+        public List<string[]> getFileContent() 
+        {
+            List<string[]> csvArray = new List<string[]>();
+            string line;
+            string[] row;
+
+            StreamReader readFile = new StreamReader(this.rutaDeArchivo);
+            while ((line = readFile.ReadLine()) != null)
+            {
+                row = line.Split(',');
+                csvArray.Add(row);
+            }
+
+            return csvArray;
+        }
+
+        public void verifyFileType()
+        {
+
+        	if(verifyPath()){
+                if (Path.GetExtension(this.rutaDeArchivo) == ".csv")
+                    Console.WriteLine("Tu archivo si es CSV");
+                else
+                    Console.WriteLine("Tu archivo no es CSV es " + Path.GetExtension(this.rutaDeArchivo) );
+        	}else{
+        		Console.WriteLine("Archivo no existe.");
+        		System.Environment.Exit(0);
+        	}
+        }
+        private Boolean verifyPath()
+        {
+            Boolean fileExist;
+            if(File.Exists(this.rutaDeArchivo))
+                fileExist = true;
+            else
+                fileExist = false;
+
+            return fileExist;
+        }
+        public void leerArchivo()
+        {
+        try 
+			{
+            Console.WriteLine("Introduce la direccion y el nombre del Archivo a Leer:");
+            this.rutaDeArchivo = Console.ReadLine();
+            				}
+				catch(Exception e)
+				{
+					Console.WriteLine("Exception: " + e.Message);
+				}
+			   	finally 
+				{
+					Console.WriteLine("Cerrando la lectura.");
+				}
+        }
+        
+    }
+}
---
+++ Practica4/Practica4/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Archivo Archivo = new Archivo();
+            Archivo.leerArchivo();
+            Archivo.verifyFileType();
+            List<string[]> myFile = Archivo.getFileContent();
+
+            foreach(string[] array in myFile)
+            {
+			try 
+				{
+                Console.WriteLine("\nId: " + array[0] + "\nNombre: " + array[1] + 
+				                  "\nDirección: " + array[2] + "\nTelefono: " + array[3]);
+								}
+				catch(Exception e)
+				{
+					Console.WriteLine("Exception: " + e.Message);
+				}
+			   	finally 
+				{
+					Console.WriteLine("Cerrando la lectura.");
+				}
+            }
+            Console.ReadKey();
+        }
+    }
+}
